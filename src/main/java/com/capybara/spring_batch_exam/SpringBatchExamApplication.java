d8157d5b2a7315adb24f3bfa614aaff6c97cbf01
eri <44278562+elyo9381@users.noreply.github.com>
2025-04-09 15:32:34 +0900

    readme 작성 및 base code template

---
+++ src/main/java/com/capybara/spring_batch_exam/SpringBatchExamApplication.java
@@ -0,0 +1,13 @@
+package com.capybara.spring_batch_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchExamApplication.class, args);
+	}
+
+}
